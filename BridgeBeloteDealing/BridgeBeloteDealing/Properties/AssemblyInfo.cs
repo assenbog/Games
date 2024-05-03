8b712a9d160296017a97a649d9baa6b084382cfc
Assen Bogdanov <assen.bogdanov@rimo3.com>
2024-05-03 09:05:24 +0100

    Added cards icon & copyright info

--- BridgeBeloteDealing/BridgeBeloteDealing/Properties/AssemblyInfo.cs
+++ BridgeBeloteDealing/BridgeBeloteDealing/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BridgeBeloteDealing")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Assen Bogdanov & Simtcho Serafimov 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- BridgeBeloteDealing/BridgeBeloteLogic/Properties/AssemblyInfo.cs
+++ BridgeBeloteDealing/BridgeBeloteLogic/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BridgeBeloteLogic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bridge Belot dealings generation program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BridgeBeloteLogic")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2024, Assen Bogdanov & Simtcho Serafimov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
